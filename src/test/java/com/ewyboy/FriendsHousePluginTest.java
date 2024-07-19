fa1962b7f3e64fe1f7d8b36a2ba90418786d826c
Eivind Norling <eivind.norling@tidsbanken.no>
2024-07-19 22:02:12 +0200

    Updated docs

--- src/test/java/com/ewyboy/FriendsHousePluginTest.java
+++ src/test/java/com/ewyboy/FriendsHousePluginTest.java
@@ -2,6 +2,9 @@ package com.ewyboy;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
 
 public class FriendsHousePluginTest
 {
@@ -10,4 +13,17 @@ public class FriendsHousePluginTest
 		ExternalPluginManager.loadBuiltin(FriendsHousePlugin.class);
 		RuneLite.main(args);
 	}
+
+	@Test
+	public void testPluginInitialization() {
+		FriendsHousePlugin plugin = new FriendsHousePlugin();
+		assertNotNull("Plugin should not be null", plugin);
+	}
+
+	@Test
+	public void testPluginName() {
+		FriendsHousePlugin plugin = new FriendsHousePlugin();
+		assertNotNull("Plugin name should not be null", plugin.getName());
+	}
+
 }
